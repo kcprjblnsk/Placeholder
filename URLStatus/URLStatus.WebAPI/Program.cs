5e35d762febad99eb3a86845ae1fd2e8e157ec90
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-04-22 18:43:21 +0200

    SeriLog added to the Program.cs

--- URLStatus/URLStatus.WebAPI/Program.cs
+++ URLStatus/URLStatus.WebAPI/Program.cs
@@ -1,10 +1,20 @@
+using Serilog;
+
 namespace URLStatus.WebAPI
 {
     public class Program
     {
+        public static string APP_NAME = "URLStatus.WebAPI";
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((context, services, configuration) =>
+                configuration.Enrich.WithProperty("Application",APP_NAME)
+                    .Enrich.WithProperty("MachineName",Environment.MachineName)
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext());
 
             // Add services to the container.
 
