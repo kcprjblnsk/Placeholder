46a9c83dd293f6c90a73186a03ccf4f8b9aaa7c7
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-04-24 13:27:01 +0200

    Added Database T-SQL Account

---
+++ Database/URLStatus.Database/dbo/Tables/AccountUsers.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[AccountUsers]
+(
+	[Id] INT IDENTITY (1,1) NOT NULL ,
+	[AccountId] INT NOT NULL,
+	[UserId] INT NOT NULL,
+	CONSTRAINT [PK_AccountUsers] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_AccountUsers_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_AccountUsers_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([Id]) ON DELETE CASCADE,
+	
+)
+
+GO
+CREATE INDEX [IX_AccountUsers_AccountId] ON [dbo].[AccountUsers] ([AccountId])
+
+GO
+
+CREATE INDEX [IX_AccountUsers_UserId] ON [dbo].[AccountUsers] ([UserId])
