e8ca227492f9a575efea7e3f1eac240ee87049b3
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-04-26 17:29:36 +0200

    Account Creation added

---
+++ URLStatus/URLStatus.Application/Logic/User/CreateUser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using URLStatus.Application.Exceptions;
+using URLStatus.Application.Interfaces;
+using URLStatus.Application.Logic.Abstractions;
+using URLStatus.Domain.Entities;
+
+namespace URLStatus.Application.Logic.User
+{
+    public static class CreateUser
+    {
+        public class Request : IRequest<Result>
+        {
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+
+        }
+        public class Result
+        {
+            public required int UserId { get; set; }
+
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            private readonly IPasswordManager _passwordManager;
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext, IPasswordManager passwordManager) : base(currentAccountProvider, applicationDbContext)
+            {
+                _passwordManager=passwordManager;
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var userExists = await _applicationDbContext.Users.AnyAsync(u => u.Email == request.Email);
+                {
+                    if (userExists)
+                    {
+                        throw new ErrorException("AccountWithThisEmailExists");
+                    }
+
+                }
+                var utcNow = DateTime.UtcNow;
+                var user = new Domain.Entities.User()
+                {
+                    RegisterDate = utcNow,
+                    Email = request.Email,
+                    HashedPassword = "",
+                };
+
+                user.HashedPassword = _passwordManager.HashPassword(request.Password);
+
+                _applicationDbContext.Users.Add(user);
+
+                var account = new Domain.Entities.Account()
+                {
+                    Name = request.Email,
+                    CreateDate = utcNow,
+
+                };
+                _applicationDbContext.Accounts.Add(account);
+
+                var accountUser = new AccountUser()
+                {
+                    Account = account,
+                    User = user,
+                };
+
+                _applicationDbContext.AccountUsers.Add(accountUser);
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Result()
+                {
+                    UserId = user.Id,
+                };
+            }
+        }
+    }
+}
