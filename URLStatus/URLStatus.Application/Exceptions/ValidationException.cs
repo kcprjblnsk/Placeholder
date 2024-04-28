a2d47861773bb58d31b61e0189d5a3b17f6a138b
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-04-28 22:25:46 +0200

    ValidationException + NuGet Packages added

---
+++ URLStatus/URLStatus.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLStatus.Application.Exceptions
+{
+    public class ValidationException
+    {
+        public List<FieldError> Errors { get; set; } = new List<FieldError>();
+
+        public class FieldError
+        {
+            public required string FieldName { get; set; }
+            public required string Error { get; set; }
+
+        }
+    }
+}
