170da27cbd0712b984c26050a18f72011c7d1af0
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-04-29 18:38:30 +0200

    LoginToAccount implementation

---
+++ URLStatus/URLStatus.Application/Logic/User/LoginToAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using URLStatus.Application.Exceptions;
+using URLStatus.Application.Interfaces;
+using URLStatus.Application.Logic.Abstractions;
+using URLStatus.Domain.Entities;
+
+namespace URLStatus.Application.Logic.User
+{
+    public static class LoginToAccount
+    {
+        public class Request : IRequest<Result>
+        {
+            public required string Email {get; set; }
+            public required string Password { get; set; }
+
+        }
+        public class Result
+        {
+           public required int UserId{get; set; }
+
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            private readonly IPasswordManager _passwordManager;
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext, IPasswordManager passwordManager) : base(currentAccountProvider, applicationDbContext)
+            {
+                _passwordManager=passwordManager;
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var user = await _applicationDbContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+                if (user != null)
+                {
+                    if (_passwordManager.VerifyPassword(user.HashedPassword, request.Password))
+                    {
+                        return new Result()
+                        {
+                            UserId = user.Id
+                        };
+                    }
+                }
+
+                throw new ErrorException("InvalidLoginOrPassword");
+            }
+        }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Email).EmailAddress();
+
+                RuleFor(x=>x.Password).NotEmpty();
+
+
+            }
+        }
+
+    }
+}
