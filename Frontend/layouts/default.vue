237a07f6e17ff8d98c9a617394ca8f8590d8759e
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-05-06 20:19:35 +0200

    Frontend layout changed

---
+++ Frontend/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Application</v-app-bar-title>
+        </v-app-bar>
+        <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
+            <!--  -->
+        </v-navigation-drawer>
+
+
+
+        <v-main>
+            <div class="pa-4">
+                <NuxtPage />
+            </div>
+
+        </v-main>
+    </v-app>
+</template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+const { mobile } = useDisplay();
+
+const drawer = ref(false);
+</script>
