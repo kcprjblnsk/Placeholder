bde0fb73f9fa35b6c372d6943602bcc723ba04ff
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-05-08 22:46:23 +0200

    light theme and dark theme added + default colors

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-        <v-app-bar>
+        <v-app-bar color="brand">
             <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
             <v-app-bar-title>Application</v-app-bar-title>
--- Frontend/plugins/vuetify.js
+++ Frontend/plugins/vuetify.js
@@ -1,10 +1,35 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/lib/util/colors'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    defaults: {
+      global: {
+      
+      },
+      Vsheet: {
+        eleveation: 4,
+
+      },
+    },
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          colors: {   
+            'brand': colors.indigo.darken4,
+            //surface: colors.brown.darken1,  
+          }
+        }, 
+        dark :{
+          colors: {
+            'brand': colors.indigo.darken4,
+          }
+        }
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
