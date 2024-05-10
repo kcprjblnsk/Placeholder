57f8705f24d57e5398b65fce6dc226396a5f7415
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-05-10 16:31:17 +0200

    LoginUI better working + error

--- Frontend/components/LoginUI.vue
+++ Frontend/components/LoginUI.vue
@@ -5,15 +5,18 @@
             <div v-if="userStore.$state.loading === true" class="pa-4 d-flex justify-center">
                 <VProgressCircular indeterminate></VProgressCircular>
             </div>
-            <VForm @submit.prevent="submit">
+            <VForm @submit.prevent="login" :disabled="loading">
                 <VCardText>
                     <v-text-field class="mb-2" variant="outlined" v-model="viewModel.email"
                         label="Email"></v-text-field>
                     <v-text-field class="mb-2" variant="outlined" v-model="viewModel.password" type="password"
                         label="Hasło"></v-text-field>
+                    <VAlert v-if="errorMsg" type="error" variant="tonal">{{ errorMsg }}</VAlert>
+
                 </VCardText>
                 <VCardActions>
-                    <v-btn class="mx-auto" color="primary" type="submit" variant="elevated">Zaloguj</v-btn>
+                    <v-btn class="mx-auto" color="primary" type="submit" variant="elevated"
+                        .loading="loading">Zaloguj</v-btn>
                 </VCardActions>
             </VForm>
         </VCard>
@@ -28,6 +31,10 @@ const show = computed(() => {
     return userStore.$state.isLoggedIn === false || userStore.$state.loading === true;
 });
 
+const errorMsg = ref("");
+const loading = ref(false);
+
+
 const viewModel = ref({
     email: '',
     password: ''
@@ -37,4 +44,24 @@ const submit = () => {
     console.log(viewModel.value);
 }
 
+const login = () => {
+    loading.value = true;
+    errorMsg.value = "";
+
+    useWebApiFetch('/User/Login', {
+        method: 'POST',
+        body: { ...viewModel.value },
+        onResponseError: ({ response }) => {
+            errorMsg.value = "Błąd logowania";
+        }
+    })
+        .then((response) => {
+            if (response.data.value) {
+                userStore.loadLoggedInUser();
+            }
+        })
+        .finally(() => {
+            loading.value = false;
+        });
+};
 </script>
