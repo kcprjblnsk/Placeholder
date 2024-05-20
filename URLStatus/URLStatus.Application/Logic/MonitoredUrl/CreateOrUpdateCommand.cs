89f98a92e07d6a3344fec26e3d207c4d378e7d5c
kcprjblnsk <kacper.jablonski99@gmail.com>
2024-05-20 19:36:04 +0200

    Create + update URL in database

---
+++ URLStatus/URLStatus.Application/Logic/MonitoredUrl/CreateOrUpdateCommand.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using URLStatus.Application.Exceptions;
+using URLStatus.Application.Interfaces;
+using URLStatus.Application.Logic.Abstractions;
+using URLStatus.Domain.Entities;
+
+namespace URLStatus.Application.Logic.MonitoredUrl
+{
+    public static class CreateOrUpdateCommand
+    {
+        public class Request : IRequest<Result>
+        {
+            public int? Id { get; set; }
+
+            public required string Url { get; set; }
+
+            public required ResultRuleSet RuleSet { get; set; }
+
+        }
+        public class Result
+        {
+            public required int Id { get; set; }
+
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {
+                
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+                Domain.Entities.MonitoredUrl? model = null;
+                if (request.Id.HasValue)
+                {
+                    model = await _applicationDbContext.MonitoredUrls.FirstOrDefaultAsync(u => u.Id == request.Id && u.AccountId == account.Id);
+                }
+                else
+                {
+                    model = new Domain.Entities.MonitoredUrl()
+                    {
+                        AccountId = account.Id
+                    };
+
+                    _applicationDbContext.MonitoredUrls.Add(model);
+                }
+
+                if (model == null)
+                {
+                    throw new UnauthorizedException();
+                }
+
+                model.Url = request.Url;
+                model.RuleSet = request.RuleSet;
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Result()
+                {
+                    Id = model.Id
+                };
+            }
+        }
+
+
+
+    }
+}
---
+++ URLStatus/URLStatus.WebAPI/Controllers/MonitoredUrlController.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.Options;
+using URLStatus.Application.Logic.Account;
+using URLStatus.Application.Logic.MonitoredUrl;
+using URLStatus.Application.Logic.User;
+using URLStatus.Infrastructure.Auth;
+using URLStatus.WebAPI.Application.Auth;
+using URLStatus.WebAPI.Application.Response;
+
+
+namespace URLStatus.WebAPI.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+
+    public class MonitoredUrlController : BaseController
+    {
+
+
+        public MonitoredUrlController(ILogger<MonitoredUrlController> logger,
+            IMediator mediator) : base(logger, mediator) //needed generic fixed
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateOrUpdate([FromBody] CreateOrUpdateCommand.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+    }
+}
